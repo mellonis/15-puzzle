f79be7f8812d272693968a49dfa00dbb0248b68d
rgilmullin <r.gilmullin@compassplus.com>
2020-04-09 22:38:25 +0500

    - Help and About pages were filled - Edited Readme - Changed application title

--- src/components/Help.js
+++ src/components/Help.js
@@ -7,7 +7,22 @@ class Help extends Component {
     render() {
         return (
             <div className="Help" onClick={this.hide}>
-                This is a help window!
+                <div className="container">
+                    <ul>
+                        <li>
+                            The object of the puzzle is to place the tiles in order by making sliding moves that use the empty space.
+                        </li>
+                        <li>
+                            To move a tile to the empty space, click on that tile. You can also move entire columns or rows.
+                        </li>
+                        <li>
+                            You can also undo your moves.
+                        </li>
+                        <li>
+                            And you can generate a new variant of the puzzle.
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
     }
