a43421ca0aa3d693b651967898347073e27a50b0
mellonis <mellonis@yandex.ru>
2024-08-31 14:20:24 +0300

    Fix code style

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,11 +1,10 @@
 import React, { useCallback } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { newLevel } from "../../services/reducers";
+import { newLevel } from '../../services/reducers';
 import About from '../about/About';
 import Board from '../board/Board';
 import ControlPanel from '../control-panel/ControlPanel';
 import Help from '../help/Help';
-
 import './Game.scss';
 
 const Game = () => {
